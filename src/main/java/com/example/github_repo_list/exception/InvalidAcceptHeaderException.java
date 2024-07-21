50f7a716d3ab9bdb210c74e6a6aef858c79e6ca0
Michal Borla <micborla@gmail.com>
2024-07-21 22:49:36 +0200

    Remove useless InvalidAcceptHeaderException

--- src/main/java/com/example/github_repo_list/exception/InvalidAcceptHeaderException.java
+++
@@ -1,7 +0,0 @@
-package com.example.github_repo_list.exception;
-
-public class InvalidAcceptHeaderException extends RuntimeException {
-    public InvalidAcceptHeaderException(String message) {
-        super(message);
-    }
-}
